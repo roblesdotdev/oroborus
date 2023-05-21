094a909db1ac63728b51a624cfde2627fd7829b5
roblesdotdev <httpsnip@gmail.com>
2023-05-20 21:31:30 -0300

    add protected route

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
               applications quickly and efficiently.
             </Balancer>
           </p>
-          <Button variant="outline">
+          <Button href="/dashboard" variant="outline">
             <div className="flex items-center gap-2">
               <span>Explore</span>
             </div>
